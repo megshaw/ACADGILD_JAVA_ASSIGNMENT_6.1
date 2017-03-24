c1fe496e39baddafb09b7d0af942643b89549354
meghana <meghana@Namsystech-Dev-01>
2017-03-24 16:08:16 +0530

    Assignment 6.1

---
+++ JavaAssignment6.1/src/Timer.java
@@ -0,0 +1,31 @@
+
+
+public class Timer {
+
+    private static class UpdaterThread extends Thread {
+
+        private final int TIMEOUT = 3000;
+
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(TIMEOUT);
+                    System.out.println("3 seconds passed");
+                } catch (InterruptedException ex) {
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Thread u = new UpdaterThread();
+            u.start();
+            while (true) {
+                System.out.println("--");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
